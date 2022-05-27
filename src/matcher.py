1f759da013db4638bb83cec9646bc1122904ba6c
Alterac8 <awong189@calpoly.edu>
2022-05-26 23:33:28 -0700

    matching function works between jobs

--- src/matcher.py
+++ src/matcher.py
@@ -1,10 +1,55 @@
 from utils.sqlconnect import get_connector
 import pprint
 import pandas as pd
+import numpy as np
 
 
-def vectorize(job_criteria):
-    pass
+def _cossim(s, w):
+    return np.sum(s * w) / (
+        np.sqrt(np.sum(np.square(s))) * np.sqrt(np.sum(np.square(w)))
+    )
+
+
+def _pearson(s, w):
+    _s_mean = np.mean(s)
+    _w_mean = np.mean(w)
+    return np.sum((s - _s_mean) * (w - _w_mean)) / (
+        np.sqrt(np.square(np.sum(s - _s_mean)))
+        * np.sqrt(np.square(np.sum(w - _w_mean)))
+    )
+
+
+def _weightcos(s, v, w):
+    return np.sum(s * v * w) / (
+        np.sqrt(np.sum(np.square(s))) * np.sqrt(np.sum(np.square(w))) * np.sum(v)
+    )
+
+
+def vectorize(group):
+    data = group[["CId", "CValue", "ImportanceRating"]]
+    max_cid = data["CId"].max()
+    # print(data)
+    values = np.zeros(shape=max_cid)
+    importances = np.ones(shape=max_cid) * 4
+
+    for i, vals in data.iterrows():
+        values[i] = vals["CValue"]
+        importances[i] = vals["ImportanceRating"]
+
+    return values, importances
+
+
+def match_jobs(index, job, other_jobs, limit=10):
+    results = []
+    values, importance = job
+
+    for i in other_jobs:
+        if i == index:
+            continue
+        _vals, _importance = other_jobs[i]
+        results.append((i, _cossim(values, _vals)))
+    results.sort(key=lambda x: x[1], reverse=True)
+    return results[:10]
 
 
 def match_desired_onet(job, other_jobs, limit=10):
@@ -20,7 +65,7 @@ if __name__ == "__main__":
     cursor = conn.cursor()
 
     cursor.execute(
-        """ SELECT Id, UserId, SurveyId, SurveyProfile, PType, CId, cValue, ImportanceRating FROM response, profile, profileCriteria WHERE response.SurveyProfile = profile.PId = profileCriteria.PId; """
+        """ SELECT Id, UserId, SurveyId, SurveyProfile, PType, CId, cValue, ImportanceRating FROM response, profile, profileCriteria WHERE response.SurveyProfile = profile.PId and profile.PId = profileCriteria.PId ORDER BY ID, CId; """
     )
     result = cursor.fetchall()
     df = pd.DataFrame(
@@ -37,6 +82,13 @@ if __name__ == "__main__":
         ],
     )
 
+    profile = {}
+
     # pprint.pprint(df)
     for i, g in df.groupby("Id"):
-        print(g)
+        profile[i] = vectorize(g.reset_index())
+
+    pprint.pprint(profile)
+
+    for k in profile.keys():
+        pprint.pprint(match_jobs(k, profile[k], profile))
