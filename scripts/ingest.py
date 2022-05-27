74c8e68b3003665baef5a0fb6f68190ff526a877
MuchQuak <loganpwilt@gmail.com>
2022-05-26 22:15:22 -0700

    edited criteria files removed non numeric ones

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -1,9 +1,11 @@
 from pprint import pprint
+from typing import Tuple
 import pandas as pd
 import MySQLdb
 import secrets
 import numpy as np
 import os
+import glob
 from dotenv import load_dotenv
 import math
 
@@ -305,7 +307,7 @@ def ingest_accountProfiles(conn, df):
 
 
 def ingest_desired_profileCriteria(conn, df):
-    print("Ingesting profileProfiles")
+    print("Ingesting desiredProfiles")
     desiredProfiles = []
     cursor = conn.cursor()
     criterion = df.keys()[3:]
@@ -359,12 +361,57 @@ def ingest_onet(conn, df):
     )
     conn.commit()
 
-def ingest_onetProfileCriteria(conn, filename:str):
-    book = pd.ExcelFile(filename, engine="openpyxl")
-    ws = pd.read_excel(book, "Sheet1")
-    print(ws)
-    for row in ws:
-        print (row)
+def ingest_onetProfileCriteria(conn):
+    criteria = []
+    values = []
+    cursor = conn.cursor()
+    jobs = conn.cursor()
+
+    for file in glob.glob(DIRPATH + "data/criteria/*.csv"):
+        fn = file.split("+")[1]
+        #name = " ".join(fn.split("(")[0].split("-"))
+        criteria.append(fn)
+
+    criteria = list(set(criteria))
+    jobs.execute("select ONetId, ONetJob, ONetProfile from onet")
+
+    c = "Decision-Making Autonomy"
+    cName = " ".join(c.split("(")[0].split("-"))
+    cursor.execute("SELECT CId FROM criteria WHERE cName = '{0}'".format(cName))
+    CId = cursor.fetchone()[0]
+    csvFiles = glob.glob(DIRPATH + "data/criteria/+{0}+*.csv".format(c))
+    for job in jobs:
+        val = 0
+        jobC = job[0]
+        jobP = job[2]
+
+        for file in csvFiles:
+            df = pd.read_csv(file)
+            rows = df.iterrows()
+            for _, row in rows:
+                if (row["Code"] == jobC):
+                    val += row[0]
+        r = tuple([CId, jobP, val, 4])
+        values.append(r)
+        print(r)
+
+    #for c in criteria:
+    #    for file in glob.glob(DIRPATH + "data/criteria/+{0}+*.csv".format(c)):
+    #        df = pd.read_csv(file)
+    #       for job in cursor:
+    #            jobCode = job[0]
+    #           jobProfile = job[2]
+    #            for _, row in df.iterrows():
+    #                print(row["Code"])
+
+            #for _, row in df.iloc[1:].iterrows():
+               #print(row)
+    #cursor.executemany(
+    #    """INSERT INTO profileCriteria (CId, PId, cValue, importanceRating) VALUES (%s, %s, %s, %s)""",
+    #    values,
+    #)
+
+    conn.commit()
 
 def ingest_data(filename: str):
     book = pd.ExcelFile(filename, engine="openpyxl")
@@ -407,8 +454,6 @@ if __name__ == "__main__":
     workprefs = pd.read_csv(DIRPATH + "data/info/WorkPrefs.csv")
     profiles = pd.read_csv(DIRPATH + "data/info/profile.csv")
     onet = pd.read_csv(DIRPATH + "data/info/All_STEM_Occupations.csv")
-    onetMapping = DIRPATH + "data/info/O-Net Computer Science Template for Survey_5_13_2022.xlsx"
-
 
     ingest_desired_profiles(conn, workprefs)
     ingest_criteria(conn, profiles)
@@ -422,6 +467,6 @@ if __name__ == "__main__":
     ingest_ure_responses(conn, sheets["URE Experience"])
     ingest_work_responses(conn, sheets["Work Experience"])
     ingest_onet(conn, onet)
-    ingest_onetProfileCriteria(conn, onetMapping)
+    ingest_onetProfileCriteria(conn)
 
     conn.close()
