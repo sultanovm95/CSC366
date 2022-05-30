fef4d2393a18ad0e5193fd2a5dcc0639634a6d69
MuchQuak <loganpwilt@gmail.com>
2022-05-29 22:29:04 -0700

    added basic get survey that only show the questions

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask
 from flask_mysqldb import MySQL
+from flask_cors import CORS
+import json
 import os
 from dotenv import load_dotenv
 
-
 app = Flask(__name__)
-mysql = MySQL(app)
-
-app = Flask(__name__)
-
+CORS(app)
 
 # Loads Enviroment Variables from .env file use command: 'touch .env' to create
 load_dotenv()
 # need to have the following variables in .env file by name
-USER = os.getenv("USER")
+USER = os.getenv("MYSQL_USER")
 PASSWORD = os.getenv("PASSWORD")
 PORT = int(os.getenv("PORT"))
 DB = os.getenv("MYSQL_DB")
+HOST = os.getenv("MYSQL_HOST")
 
 app.config.setdefault("MYSQL_HOST", "localhost")
 app.config.setdefault("MYSQL_USER", USER)
@@ -33,6 +32,7 @@ app.config.setdefault("MYSQL_CURSORCLASS", None)
 app.config.setdefault("MYSQL_AUTOCOMMIT", False)
 app.config.setdefault("MYSQL_CUSTOM_OPTIONS", None)
 
+mysql = MySQL(app)
 
 @app.route("/")
 def dbUsers():
@@ -41,6 +41,24 @@ def dbUsers():
     rv = cur.fetchall()
     return str(rv)
 
+@app.route("/survey/URE")
+def getSurvey():
+    cur = mysql.connection.cursor()
+
+    cur.execute(
+        """
+        select SurveyId, ShortName, q.Id, Question, QuestionType, ProfileCharacteristic 
+        from survey as s 
+        join question as q on s.Id = SurveyId 
+        where s.ShortName = 'URE Experience'
+        """)
+
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+    return json.dumps(json_data)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000, debug=True)
