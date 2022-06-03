9967eaa5d58040578159e327aa8d9305158b523a
MuchQuak <loganpwilt@gmail.com>
2022-06-03 14:16:51 -0700

    added profile job matching with /match/profile?id=<profile id>

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from cmath import pi
 import MySQLdb
+from src.matcher import match
+from src.utils.sqlconnect import get_connector
 from flask import Flask, request
 from flask_mysqldb import MySQL
 from flask_cors import CORS
@@ -41,6 +44,23 @@ def dbUsers():
     cur.execute("select user,host from mysql.user")
     rv = cur.fetchall()
     return str(rv)
+
+@app.route("/match/profile")
+def getJobMatches(pid=0):
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    pid = request.args.get('id', type = int)
+
+    cur.execute("select ONetId, ONetJob, ONetDescription from onet")
+
+    matchedJobs = []
+    matches = match(pid)
+    for m in matches:
+        onetId = m[0]
+        for job in cur:
+            if(job["ONetId"] == onetId):
+                matchedJobs.append(job)
+
+    return json.dumps({"PId": pid, "matches": matchedJobs})
 
 @app.route("/user/<aid>/profiles")
 def getUserProfile(aid=0):
