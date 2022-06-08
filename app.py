604e7438206d2fc3b6311a83278e6c4d803de611
Alterac8 <awong189@calpoly.edu>
2022-06-07 22:07:15 -0700

    refomat and adding delete

--- app.py
+++ app.py
@@ -3,14 +3,21 @@ import MySQLdb
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 from flask_cors import CORS
+from flask_jwt_extended import jwt_required, get_jwt_identity
 import json
 import os
 import jwt
-from src import queries
+from src import operations, queries
 from dotenv import load_dotenv
 from functools import wraps
 from src.user import User
-from src.matcher import match, getONetJobs, getVectorizedProfile, match_desired_onet, match_exp_onet
+from src.matcher import (
+    match,
+    getONetJobs,
+    getVectorizedProfile,
+    match_desired_onet,
+    match_exp_onet,
+)
 
 app = Flask(__name__)
 CORS(app)
@@ -18,7 +25,7 @@ CORS(app)
 # Loads Enviroment Variables from .env file use command: 'touch .env' to create
 load_dotenv()
 # need to have the following variables in .env file by name
-app.config['USER_SECRET'] = os.getenv('USER_SECRET')
+app.config["USER_SECRET"] = os.getenv("USER_SECRET")
 USER = os.getenv("MYSQL_USER")
 PASSWORD = os.getenv("PASSWORD")
 PORT = int(os.getenv("PORT"))
@@ -42,19 +49,21 @@ app.config.setdefault("MYSQL_CUSTOM_OPTIONS", None)
 
 mysql = MySQL(app)
 
+
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = request.args.get('token')
+        token = request.args.get("token")
         if not token:
-            return jsonify({'message': 'Token is missing!'}), 401
+            return jsonify({"message": "Token is missing!"}), 401
         try:
-            data = jwt.decode(token, app.config['USER_SECRET'])
+            data = jwt.decode(token, app.config["USER_SECRET"])
         except:
-            return jsonify({'message': 'Invalid token'}), 401
+            return jsonify({"message": "Invalid token"}), 401
         return f(data, *args, kwargs)
+
     return decorated
-        
+
 
 @app.route("/")
 def dbUsers():
@@ -67,30 +76,32 @@ def dbUsers():
 @app.route("/criteria_values")
 def getCriteriaValues():
     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cur.execute("select CId,cName,cDescription,4 as cValue,4 as importanceRating from criteria")
+    cur.execute(
+        "select CId,cName,cDescription,4 as cValue,4 as importanceRating from criteria"
+    )
     return json.dumps({"criteria": cur.fetchall()})
 
 
-@app.route("/profile", methods=['GET', 'POST', 'PATCH'])
+@app.route("/profile", methods=["GET", "POST", "PATCH"])
 @token_required
 def profile():
     conn = mysql.connect
     try:
-        if request.method == 'GET':
+        if request.method == "GET":
             pid = request.args.get("pid", type=int)
             if pid == None:
                 return {"Error": "pid not provided"}, 400
 
             return queries.getProfile(conn, pid), 200
-            #return getProfile(conn, pid)
+            # return getProfile(conn, pid)
 
-        elif request.method == 'POST':
+        elif request.method == "POST":
             aid = request.args.get("aid", type=int)
             if aid == None:
                 return {"Error": "Profile POST requires an aid"}, 400
             return queries.addDesiredProfile(conn, aid, request.json)
 
-        elif request.method == 'PATCH':
+        elif request.method == "PATCH":
             pid = request.args.get("pid", type=int)
             if pid == None:
                 return {"Error": "pid not provided"}, 400
@@ -102,24 +113,24 @@ def profile():
         conn.close()
 
 
-@app.route("/profile/match", methods=['GET', 'POST'])
+@app.route("/profile/match", methods=["GET", "POST"])
 @token_required
 def profileMatch(pid=0):
 
     conn = mysql.connect
     try:
-        if request.method == 'GET':
+        if request.method == "GET":
             pid = request.args.get("pid", type=int)
             if pid == None:
                 return {"Error": "pid not provided"}, 400
             return queries.getJobMatches(conn, pid)
-        elif request.method == 'POST':
+        elif request.method == "POST":
             return queries.postJobMatches(conn, {"Profile": "Not Actual"})
     finally:
         conn.close()
 
 
-@app.route("/profile/user", methods=['GET', 'POST'])
+@app.route("/profile/user", methods=["GET", "POST"])
 @token_required
 def userProfile():
     conn = mysql.connect
@@ -127,10 +138,10 @@ def userProfile():
         aid = request.args.get("aid", type=int)
         if aid == None:
             return {"Error": "aid not provided"}, 400
-        if request.method == 'GET':
+        if request.method == "GET":
             return queries.getUserProfiles(conn, aid)
-        elif request.method == 'POST':
-            #for getting matches with profile json templates
+        elif request.method == "POST":
+            # for getting matches with profile json templates
             return queries.postUserProfiles(conn, aid, request.json)
         else:
             return "{0} not an implemented method".format(request.method)
@@ -145,12 +156,13 @@ def userProfile():
 def profileTemplate():
     conn = mysql.connect
     try:
-        if request.method == 'GET':
+        if request.method == "GET":
             return json.dumps(queries.getTemplate(conn))
     except Exception as e:
         return str(e), 500
     finally:
         conn.close()
+
 
 @app.route("/jobs")
 # @token_required
@@ -166,14 +178,14 @@ def getSurveys():
     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cur.execute("select Id, ShortName, Name, Description from survey")
     return json.dumps({"surveys": cur.fetchall()})
-    
+
 
 @app.route("/survey")
 @token_required
 def survey():
     conn = mysql.connect
     try:
-        if request.method == 'GET':
+        if request.method == "GET":
             sid = request.args.get("sid", type=int)
             if sid == None:
                 return {"Error": "survey sid not provided"}, 400
@@ -185,7 +197,8 @@ def survey():
     finally:
         conn.close()
 
-@app.route("/response", methods=['GET', 'POST'])
+
+@app.route("/response", methods=["GET", "POST"])
 @token_required
 def response():
     conn = mysql.connect
@@ -193,9 +206,9 @@ def response():
         aid = request.args.get("aid", type=int)
         if aid == None:
             return {"Error": "Account aid not provided"}, 400
-        if request.method == 'GET':
+        if request.method == "GET":
             return queries.getResponse(conn, aid)
-        elif request.method == 'POST':
+        elif request.method == "POST":
             body = request.json
             if body == None:
                 return {"Error": "No response payload sent"}, 400
@@ -204,6 +217,7 @@ def response():
         return str(e), 500
     finally:
         conn.close()
+
 
 @app.route("/match")
 @token_required
@@ -225,43 +239,66 @@ def getMatch():
     print(matches)
     return {"matches": matches}
 
+
 @app.route("/users/signup", methods=["POST"])
 def signup():
     if request.method == "POST":
         user = {}
-        user['name'] = request.json['firstName'] + " " + request.json['lastName']
-        user['email'] = request.json['email']
-        user['password'] = request.json['password']
-        user['account_type'] = 'user'
-        
+        user["name"] = request.json["firstName"] + " " + request.json["lastName"]
+        user["email"] = request.json["email"]
+        user["password"] = request.json["password"]
+        user["account_type"] = "user"
+
         u = User()
         if u.check_user(user):
             return {"Error": "User already exists"}, 500
         else:
             u.create_user(user)
-            payload = {'name': user['name'],
-                       'email': user['email'],
-                       'account_type': user['account_type'],
-                       'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)}
-            token = jwt.encode(payload, app.config['USER_SECRET'])
-            return jsonify({'token': token.decode('UTF-8')}), 201
+            payload = {
+                "name": user["name"],
+                "email": user["email"],
+                "account_type": user["account_type"],
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            }
+            token = jwt.encode(payload, app.config["USER_SECRET"])
+            return jsonify({"token": token.decode("UTF-8")}), 201
 
 
 @app.route("/users/login", methods=["POST"])
 def login():
     if request.method == "POST":
         user = {}
-        user['email'] = request.form['email']
-        user['password'] = request.form['password']
-        
+        user["email"] = request.form["email"]
+        user["password"] = request.form["password"]
+
         u = User()
         if u.verify_user(user):
-            payload = {'email': user['email'],
-                       'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)}
-            token = jwt.encode(payload, app.config['USER_SECRET'])
-            return jsonify({'token': token.decode('UTF-8')})
+            payload = {
+                "email": user["email"],
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            }
+            token = jwt.encode(payload, app.config["USER_SECRET"])
+            return jsonify({"token": token.decode("UTF-8")})
         else:
             return {"Error": "User not found"}, 500
+
+@app.route("/delete", methods=["POST"])
+# @jwt_required()
+def delete():
+    if request.method == "POST":
+        curr_user = 1
+        # curr_user = get_jwt_identity()
+        profile_id = request.json.get("pid")
+
+        if profile_id:
+            result = operations.delete_profile(mysql.connection, curr_user, profile_id=profile_id)
+
+            if result == -1:
+                return {"Error": "UserId doesn't own profileId"}
+
+            return {"delete": profile_id}
+        return {"Error", "ProfileId not found"}, 500
+    return {"Error": "Post request only"}, 500
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000, debug=True)
--- src/queries.py
+++ src/queries.py
@@ -3,17 +3,20 @@ import MySQLdb
 import json
 from src import matcher
 
+
 def retrieveProfileCriteria(conn, pid):
     cur = conn.cursor(MySQLdb.cursors.DictCursor)
     try:
         cur.execute(
-        f"SELECT profile.pid, ptype, pname, criteria.cName, criteria.cid, cvalue, importanceRating \
+            f"SELECT profile.pid, ptype, pname, criteria.cName, criteria.cid, cvalue, importanceRating \
             FROM profile, profileCriteria, criteria \
             WHERE profileCriteria.PId = profile.PId AND profile.pid = {pid} AND criteria.cid = profileCriteria.cid\
-            ")
-        return(json.dumps({"PId": pid, "Criteria": cur.fetchall()}))
+            "
+        )
+        return json.dumps({"PId": pid, "Criteria": cur.fetchall()})
     finally:
         cur.close()
+
 
 def getProfile(conn, pid):
     cur = conn.cursor(MySQLdb.cursors.DictCursor)
@@ -25,13 +28,22 @@ def getProfile(conn, pid):
             select criteria.CId, cName, cValue, importanceRating from profileCriteria
             join criteria on criteria.CId = profileCriteria.CId
             where PId = %(PId)s;
-            """, 
-            {"PId": pid})
+            """,
+            {"PId": pid},
+        )
 
         criteria = cur.fetchall()
-        return json.dumps({"PId": pid, "PName": pro["PName"],"PType": pro["PType"], "Criteria": criteria})
+        return json.dumps(
+            {
+                "PId": pid,
+                "PName": pro["PName"],
+                "PType": pro["PType"],
+                "Criteria": criteria,
+            }
+        )
     finally:
         cur.close()
+
 
 def addDesiredProfile(conn, aid, body):
     try:
@@ -42,7 +54,8 @@ def addDesiredProfile(conn, aid, body):
             """
             INSERT INTO accountProfile (AId, PId) VALUES (%(AId)s, %(PId)s)
             """,
-        {"AId": aid, "PId": PId})
+            {"AId": aid, "PId": PId},
+        )
 
         conn.commit()
         cur.close()
@@ -54,13 +67,14 @@ def addDesiredProfile(conn, aid, body):
         conn.rollback()
         return e.args[0], e.args[1]
 
+
 def checkProfileBody(body):
     keys = body.keys()
-    if 'PName' not in keys:
+    if "PName" not in keys:
         raise Exception("'PName' not included in the request", 400)
-    if 'PType' not in keys:
+    if "PType" not in keys:
         raise Exception("'PType' not included in the request", 400)
-    if 'Criteria' not in keys:
+    if "Criteria" not in keys:
         raise Exception("'Criteria' not included in the request", 400)
 
     def _0to7(value):
@@ -69,49 +83,65 @@ def checkProfileBody(body):
         elif value < 0:
             return 0
         return value
-    
-    #Assuming Valid CId
+
+    # Assuming Valid CId
     for c in body["Criteria"]:
         cKeys = c.keys()
-        if ('cValue' not in cKeys or 'CId' not in cKeys or 'importanceRating' not in cKeys):
-            raise Exception("""
+        if (
+            "cValue" not in cKeys
+            or "CId" not in cKeys
+            or "importanceRating" not in cKeys
+        ):
+            raise Exception(
+                """
                 'Criteria' must have json with 'CId', 'cValue', and 'importanceRating' keys
-                """, 400)
+                """,
+                400,
+            )
         c["cValue"] = _0to7(c["cValue"])
         c["importanceRating"] = _0to7(c["importanceRating"])
+
 
 def addProfile(conn, body, PType):
     cur = conn.cursor(MySQLdb.cursors.DictCursor)
     cur.execute("select max(PId) + 1 as PId from profile")
     r = cur.fetchone()
     PId = r["PId"]
-    
+
     try:
         if PId == None:
             raise Exception("DB couldn't generate a PId", 500)
-        
+
         checkProfileBody(body)
-        
+
         cur.execute(
             """
             INSERT INTO profile (PId, PName, PType) VALUES (%(PId)s, %(PName)s, %(PType)s)
-            """, 
-            {"PId": PId, "PName": body["PName"], "PType": PType})
+            """,
+            {"PId": PId, "PName": body["PName"], "PType": PType},
+        )
 
         cur.executemany(
             """
             INSERT INTO profileCriteria (CId, PId, cValue, importanceRating) 
             VALUES (%(CId)s, {0}, %(cValue)s, %(importanceRating)s)
-            """.format(int(PId))
-        , body["Criteria"])
+            """.format(
+                int(PId)
+            ),
+            body["Criteria"],
+        )
 
-        #Intentionally doesn't commit leave it up to parent call
-        return {"PId": int(PId), "Msg": "Successfully added profile {0}".format(PId)}, 201
+        # Intentionally doesn't commit leave it up to parent call
+        return {
+            "PId": int(PId),
+            "Msg": "Successfully added profile {0}".format(PId),
+        }, 201
     except MySQLdb.DatabaseError as e:
         conn.rollback()
         return str(e), 500
     finally:
         cur.close()
+
 
 def updateProfile(conn, pid, body):
 
@@ -119,7 +149,7 @@ def updateProfile(conn, pid, body):
 
     if body["PType"] != "Desired":
         raise Exception("You can only update desired profiles", 400)
-    
+
     try:
         cur = conn.cursor(MySQLdb.cursors.DictCursor)
         cur.execute(
@@ -127,16 +157,20 @@ def updateProfile(conn, pid, body):
             UPDATE profile
             SET PName = %(PName)s
             WHERE PId = %(PId)s
-            """, 
-            {"PId": pid, "PName": body["PName"]})
+            """,
+            {"PId": pid, "PName": body["PName"]},
+        )
 
         cur.executemany(
             """
             UPDATE profileCriteria 
             SET cValue = %(cValue)s, importanceRating = %(importanceRating)s
             WHERE PId = {0} AND CId = %(CId)s
-            """.format(int(pid)), 
-            body["Criteria"])
+            """.format(
+                int(pid)
+            ),
+            body["Criteria"],
+        )
 
         conn.commit()
         return {"Msg": "Successfully updated profile {0}".format(pid)}, 200
@@ -145,6 +179,7 @@ def updateProfile(conn, pid, body):
         return str(e), 500
     finally:
         cur.close()
+
 
 def getJobMatches(conn, pid):
 
@@ -164,14 +199,16 @@ def getJobMatches(conn, pid):
     finally:
         cur.close()
 
+
 def postJobMatches(conn, profileJson):
     try:
         cur = conn.cursor()
         checkProfileBody(profileJson)
-        #matcher.vectorize(profileJson)
+        # matcher.vectorize(profileJson)
         return "matching based on json post not implemented"
     finally:
         cur.close()
+
 
 def createSurvey(surveyQ, questionA):
     json_data = {"surveyId": surveyQ[0][0], "surveyName": surveyQ[0][1], "elements": []}
@@ -194,6 +231,7 @@ def createSurvey(surveyQ, questionA):
         json_data["elements"].append(r)
     return json.dumps(json_data)
 
+
 def getUserProfiles(conn, aid):
     try:
         cur = conn.cursor(MySQLdb.cursors.DictCursor)
@@ -206,15 +244,18 @@ def getUserProfiles(conn, aid):
             join profile on userProfiles.PId = profile.PId 
             where AId = %(AId)s
             """,
-            {"AId": aid},)
-        return json.dumps({"AId": aid,"profiles": cur.fetchall()})
+            {"AId": aid},
+        )
+        return json.dumps({"AId": aid, "profiles": cur.fetchall()})
     except:
         return "Error: Couldn't GET user Profiles"
     finally:
         cur.close()
 
+
 def postUserProfiles(conn, aid, body):
     return json.dumps(body)
+
 
 def getTemplate(conn):
     cur = conn.cursor(MySQLdb.cursors.DictCursor)
@@ -226,13 +267,23 @@ def getTemplate(conn):
 
         profileCriteria = []
         for c in criterion:
-            cp = {"CId": c["CId"],"cName": c["cName"], "cValue": 0, "importanceRating": 4}
+            cp = {
+                "CId": c["CId"],
+                "cName": c["cName"],
+                "cValue": 0,
+                "importanceRating": 4,
+            }
             profileCriteria.append(cp)
 
-        profileTemplate = {"PName": "Template", "PType": "Desired","Criteria": profileCriteria}
+        profileTemplate = {
+            "PName": "Template",
+            "PType": "Desired",
+            "Criteria": profileCriteria,
+        }
         return profileTemplate
     finally:
         cur.close()
+
 
 def getSurvey(conn, sid):
     try:
@@ -258,6 +309,7 @@ def getSurvey(conn, sid):
     finally:
         cur.close()
 
+
 def createQuestion(num, type, prompt, choices):
     qtype = ["dropdown", "matrix"]
     if type == 1:
@@ -276,15 +328,17 @@ def createQuestion(num, type, prompt, choices):
             "title": prompt,
             "isRequired": True,
             "choices": choices,
-        }    
+        }
+
 
 def getResponse(conn, aid):
     try:
         cur = conn.cursor(MySQLdb.cursors.DictCursor)
         cur.execute("select * from response where UserId = %(AId)s", {"AId": aid})
-        return json.dumps({"AId": aid,"Responses": cur.fetchall()})
+        return json.dumps({"AId": aid, "Responses": cur.fetchall()})
     finally:
         cur.close()
+
 
 def generateResponseProfile(conn, qMap):
     profile = getTemplate(conn)
@@ -293,19 +347,20 @@ def generateResponseProfile(conn, qMap):
         cValue = 0
         cnt = 0
         for q in qMap:
-            #maybe check same with qMap[a]["qNum"] == a
+            # maybe check same with qMap[a]["qNum"] == a
             if q["ProfileCharacteristic"] == cName and "cValue" in q:
                 cValue += q["cValue"]
                 cnt += 1
         if cnt > 0:
             c["cValue"] = cValue / cnt
-    
-    return profile   
+
+    return profile
+
 
 def addResponse(conn, aid, body):
 
-    #body = {"usedId": "123ABC", "SId": 1, "answers": {1: 10, 2: 1, 3: 3, 4: 3, 5: 2, 6: 1, 7: 2}}
-    
+    # body = {"usedId": "123ABC", "SId": 1, "answers": {1: 10, 2: 1, 3: 3, 4: 3, 5: 2, 6: 1, 7: 2}}
+
     try:
         sid = body["SId"]
         cur = conn.cursor(MySQLdb.cursors.DictCursor)
@@ -314,18 +369,20 @@ def addResponse(conn, aid, body):
             select Id as qNum, ProfileCharacteristic 
             from question 
             where SurveyId = %(SId)s
-            """, {"SId": sid})
+            """,
+            {"SId": sid},
+        )
 
         qMap = cur.fetchall()
         answers = body["answers"]
 
         for q in qMap:
             q["cValue"] = 0
-        
+
         for key in answers:
             qNum = int(key)
             qMap[qNum]["cValue"] = answers[key]
-        
+
         profile = generateResponseProfile(conn, qMap)
         pid = addProfile(conn, profile, "Experience")[0]["PId"]
 
@@ -333,16 +390,22 @@ def addResponse(conn, aid, body):
         rid = cur.fetchone()["RId"]
 
         cur.execute(
-        """
+            """
         INSERT INTO response (Id, UserId, SurveyId, SurveyProfile, AnswerDate) 
         VALUES (%(RId)s, %(AId)s, %(SId)s, %(PId)s, NOW())
-        """, {"RId": rid, "AId": aid, "SId": sid, "PId": pid})
+        """,
+            {"RId": rid, "AId": aid, "SId": sid, "PId": pid},
+        )
 
         cur.executemany(
-        """
+            """
         INSERT INTO answers (QuestionId, ResponseId, AnswerValue) 
         VALUES (%(qNum)s, {0}, %(cValue)s)
-        """.format(rid), qMap)
+        """.format(
+                rid
+            ),
+            qMap,
+        )
 
         conn.commit()
         return {"Msg": "Successfully added survey response"}, 201
